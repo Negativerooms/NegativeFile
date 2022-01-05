65d4e24affaaf30982eaab5b80f95e58ee99a007
Negativerooms <96872579+Negativerooms@users.noreply.github.com>
2022-01-05 17:41:32 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -72,7 +72,7 @@ async def start_command(client: Client, message: Message):
             except BaseException:
                 return
         temp_msg = await message.reply("😈")
-                 = await message.reply("<code>HelLow</code>")
+        temp_msg = await message.reply("<code>HelLow</code>")
         try:
             messages = await get_messages(client, ids)
         except BaseException:
